34ede767aa49468dc130cf5d0a5646281ded8151
test <shino.ko.251105@gmail.com>
2026-05-29 10:10:04 +0900

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -4,5 +4,5 @@ document.body.appendChild(glow);
 
 document.addEventListener('mousemove', e => {
     glow.style.left = e.clientX + 'px';
-    glow.style.top  = e.clientY + 'px';
+    glow.style.top = e.clientY + 'px';
 });
