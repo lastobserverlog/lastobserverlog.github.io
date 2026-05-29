57f10e77677965d1db557394ab765527d3fe8b3b
test <shino.ko.251105@gmail.com>
2026-05-29 10:06:17 +0900

    Create mouse.js

---
+++ mouse.js
@@ -0,0 +1,8 @@
+const glow = document.createElement('div');
+glow.className = 'cursor-glow';
+document.body.appendChild(glow);
+
+document.addEventListener('mousemove', e => {
+    glow.style.left = e.clientX + 'px';
+    glow.style.top  = e.clientY + 'px';
+});
