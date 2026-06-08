4eb566bc30cf99fa9c8bd0d39346dfec21a3d9d7
test <shino.ko.251105@gmail.com>
2026-06-08 09:25:43 +0900

    Create soul.js

---
+++ soul.js
@@ -0,0 +1,22 @@
+<body>
+
+<div class="refraction"></div>
+
+<div class="cursor-soul" id="soul">
+    <div class="soul-core"></div>
+    <div class="soul-tail"></div>
+</div>
+
+<main class="glass-wrap">
+    <pre class="record" id="recordText">
+ここに本文
+    </pre>
+
+    <span class="signature" id="sig">
+        ― 忠之
+    </span>
+</main>
+
+<script src="assets/js/soul.js"></script>
+
+</body>
